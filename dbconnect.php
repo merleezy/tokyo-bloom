1ebed2fb5b70aad7dd0ac817f4b98229a0870856
Isaac <isaacmcd03@gmail.com>
2025-11-18 09:54:07 -0500

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$database = "tokyo_bloom";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO(dsn: "mysql:host=$servername; dbname=$database", username: $username, password: $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+  echo "<br>";
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
